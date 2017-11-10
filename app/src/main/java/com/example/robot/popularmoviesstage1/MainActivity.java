745026ee71020b57a1918c84aea0faf5728cd395
Rob Quick <requick1031@gmail.com>
2017-11-10 14:54:24 +0900

    by god i think i've got it now

--- app/src/main/java/com/example/robot/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/robot/popularmoviesstage1/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // why isn't this showing up
+        // ah okay here we go
     }
 }
